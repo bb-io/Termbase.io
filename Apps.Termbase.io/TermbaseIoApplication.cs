9ce3e24506e3a1df8049aed94a668c332fb42d43
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-04-11 17:49:45 +0300

    Added categories

---
+++ Apps.Termbase.io/TermbaseIoApplication.cs
@@ -0,0 +1,24 @@
+﻿using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Metadata;
+
+namespace Apps.Termbase.io;
+
+public class TermbaseIoApplication :  IApplication, ICategoryProvider
+{
+    public IEnumerable<ApplicationCategory> Categories
+    {
+        get => new[] { ApplicationCategory.ArtificialIntelligence };
+        set { }
+    }
+    
+    public string Name
+    {
+        get => "termbase.io";
+        set { }
+    }
+    
+    public T GetInstance<T>()
+    {
+        throw new NotImplementedException();
+    }
+}
