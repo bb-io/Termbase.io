08909487fc865c647ef989f04e1c7931c170af4d
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-04-12 17:24:55 +0300

    Added static data source handler

--- Apps.Termbase.io/Models/Request/ExportFileRequest.cs
+++ Apps.Termbase.io/Models/Request/ExportFileRequest.cs
@@ -1,10 +1,12 @@
-﻿using Blackbird.Applications.Sdk.Common;
+﻿using Apps.Termbase.io.DataSourceHandlers.StaticDataSourceHandlers;
+using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dictionaries;
 
 namespace Apps.Termbase.io.Models.Request;
 
 public class ExportFileRequest
 {
-    [Display("Format")]
+    [Display("Format"), StaticDataSource(typeof(ExportFormatStaticDataSourceHandler))]
     public string? Format { get; set; }
     
     [Display("Skip empty terms")]
