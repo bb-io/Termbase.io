ad4ea69509765e84568d4b0c159f3daf2b96ee08
KaiPiontek <kai.piontek@termbase.io>
2024-12-15 20:49:48 +0100

    Update Urls.cs

--- Apps.Termbase.io/Constants/Urls.cs
+++ Apps.Termbase.io/Constants/Urls.cs
@@ -3,8 +3,7 @@
 public static class Urls
 {
 
-    //public const string SystemUrl = "https://app.termbase.io";
-    public const string SystemUrl = "http://localhost:8000";
+    public const string SystemUrl = "https://app.termbase.io";
     public const string ApiBase = "/api";
     
     public const string Api = SystemUrl + ApiBase;
